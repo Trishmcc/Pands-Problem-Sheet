2502606beaaffe6e399ee8a50d2cb532d0d7dd51
Trish <G00398785@gmit.ie>
2021-02-15 19:31:38 +0000

    task3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pands-Problem-Sheet
 Week 2 bmi.py completed
+Week 3 secondString.py completed
+Week 4 collatz.py completed
--- bmi.py
+++ bmi.py
@@ -14,12 +14,14 @@ heightmetre = height/100
 metreSq = heightmetre**2
 
 # Calculating the BMI. 
-# The BMI will be rounded to the nearest 2 decimal places hence round and 2 were used. 
+# The BMI will be rounded to the nearest 2 decimal places 
+# hence round and 2 were used. 
 # I found this in w3resource.
 
 BMI = round(weight / metreSq, 2)
 
 # BMI output.
-# The + and str were used to make the strings and integers work together.
+# The + and str were used to make the strings and integers 
+# work together.
 
 print ("BMI is " + str(BMI))
