efe811d7aec4ab214dc4e393d03bbc046c95685a
Trish <G00398785@gmit.ie>
2021-03-08 18:29:51 +0000

    Task 7

---
+++ es.py
@@ -0,0 +1,33 @@
+# A program that reads in a text file and outputs the number of e's it contains
+# Author: Trish O'Grady
+
+# sys imports the internal Python module 
+
+import sys
+
+ # First I will open a file using the open function I'm using rt mode to read a text file
+
+
+
+
+with open("moby-dict.txt", "rt") as f:
+
+# I'm using a while loop to perform a Boolean. I'm choosing 1 so every element will be examined
+
+
+    while x == f.read(1):
+        if x == "e" or x == "E":
+            return??????
+
+# y is everything that isn't an e and the +1 will add every e found in the text
+        
+        else:
+            y = (0 + 1)?????
+
+            # argv.py
+
+
+print(f"Name of the script      : {sys.argv[0]=}")
+print(f"Arguments of the script : {sys.argv[+1]=}")
+
+#print (x)
